cfffca0c89e5b2de6db2d534f9903605e01120bd
smartdev007 <kevadiajenish7@gmail.com>
2019-05-03 18:45:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,41 +2,52 @@ let twitterQuote = document.getElementById('twitter-quote');
 let tumblrQuote = document.getElementById('tumblr-quote');
 let newQuote = document.getElementById('new-quote');
 let tweetBtn = document.getElementById('twitter-quote');
+let tumblrBtn = document.getElementById('tumblr-quote');
 
 let quoteBox = document.getElementById('quote-box');
 
-newQuote.addEventListener('click', getRandomQuote);
-tweetBtn.addEventListener('click', tweetQuote);
-
-function getRandomQuote() {
+function setupQuotes() {
     fetch('./quotes.json')
     .then(response => response.json())
     .then(data => {
 
-        if(quoteBox.classList.contains('slide')){
-            quoteBox.classList.remove('slide');
-        }
-
-        let randomInt = parseInt(Math.random() * data.length);
-
-        let quote = data[randomInt].quote;
-        let author = data[randomInt].author;
-        let img = data[randomInt].url;
+        function getRandomQuote() {
+            if (quoteBox.classList.contains('slide')) {
+                quoteBox.classList.remove('slide')
+            }
+            
+            const randomIdx = parseInt(Math.random() * data.length);
+            const { quote, author, url } = data[randomIdx];
+            
+            tweetBtn.setAttribute('href', `https://twitter.com/intent/tweet?text=${quote}`);
+            tumblrBtn.setAttribute('href', `https://twitter.com/intent/tweet?text=${quote}`);
         
-        document.getElementById('text').innerHTML = `<span class="big-quote">&#8220;</span>
-        ${quote}`;
-        document.getElementById('author').innerHTML = `-${author}`;
-
-        document.body.style.background = `url('${img}') no-repeat center top/cover`;
-
-        setTimeout(() => quoteBox.classList.add('slide'), 200);
-    })
+            setTimeout(() => {
+                document.getElementById('text').innerHTML = `<span class="big-quote">&#8220;</span>
+                ${quote}`;
+                document.getElementById('author').innerHTML = `-${author}`;
+                document.body.style.background = `url('${url}') no-repeat center top/cover`;
+                quoteBox.classList.add('slide');
+            }, 500)
+          }
+        
+          // preload images
+          const images = data.reduce((acc, { url }) => {
+                const img = new Image();
+                    img.src = url;
+                
+                if (acc.includes(img)) {
+                    return false;
+                }
+                
+                return [...acc, img];
+          }, [])
+          
+          newQuote.addEventListener('click', getRandomQuote);
+        
+          getRandomQuote();
+        })
     .catch(e => console.log(e));
 }
 
-function tweetQuote() {
-    let tweetText = document.getElementById('text').innerText;
-    tweetBtn.setAttribute("href", 'https://twitter.com/intent/tweet?text=' + tweetText);
-}
-
-document.addEventListener('DOMContentLoaded', getRandomQuote);
+document.addEventListener('DOMContentLoaded', setupQuotes);
